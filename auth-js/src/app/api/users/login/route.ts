fbf369c22a15da2ff236595bd4cd6c85fcf2f1c7
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-03-31 09:46:58 +0000

    middleware added

--- auth-js/src/app/api/users/login/route.ts
+++ auth-js/src/app/api/users/login/route.ts
@@ -3,7 +3,6 @@ import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-
 connect()
 
 export async function POST(request: NextRequest) {
--- auth-js/src/app/profile/[id]/page.tsx
+++ auth-js/src/app/profile/[id]/page.tsx
@@ -1,12 +1,18 @@
-export default async function UserProfile({ params }: { params: { id: string } }) {
+export default async function UserProfile({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr />
-            <p className="text-4xl">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+            <h1 className="text-xl mb-4">Profile</h1>
+
+            <p className="text-4xl flex items-center gap-2">
                 Profile page
-                <span className="p-2 ml-2 rounded bg-orange-500 text-black">
-                    {params.id}
+                <span className="px-3 py-1 rounded-lg bg-orange-500 text-white font-semibold">
+                    {id}
                 </span>
             </p>
         </div>
